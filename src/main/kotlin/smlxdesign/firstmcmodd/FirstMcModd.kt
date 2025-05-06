33d9120cd09fc0ae177dcd34133a8013ff64af7a
Samuel <smlxdesign@duck.com>
2025-05-06 20:56:04 +0200

    Format project

--- src/main/kotlin/smlxdesign/firstmcmodd/FirstMcModd.kt
+++ src/main/kotlin/smlxdesign/firstmcmodd/FirstMcModd.kt
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer
 import org.slf4j.LoggerFactory
 
 object FirstMcModd : ModInitializer {
-    private val logger = LoggerFactory.getLogger("first-mc-modd")
+	private val logger = LoggerFactory.getLogger("first-mc-modd")
 
 	override fun onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
--- src/main/kotlin/smlxdesign/firstmcmodd/ModItems.kt
+++ src/main/kotlin/smlxdesign/firstmcmodd/ModItems.kt
@@ -9,11 +9,15 @@ import net.minecraft.util.Identifier
 import java.util.function.Function
 
 class ModItems {
-    fun register(name: String, itemFactory: Function<Item.Settings, Item>, settings: Item.Settings): Item {
-        val itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of("smlxdesign.first-mc-modd", name))
-        val item = itemFactory.apply(settings.registryKey(itemKey))
-        Registry.register(Registries.ITEM, itemKey, item)
+	fun register(
+		name: String,
+		itemFactory: Function<Item.Settings, Item>,
+		settings: Item.Settings
+	): Item {
+		val itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of("smlxdesign.first-mc-modd", name))
+		val item = itemFactory.apply(settings.registryKey(itemKey))
+		Registry.register(Registries.ITEM, itemKey, item)
 
-        return item
-    }
+		return item
+	}
 }
